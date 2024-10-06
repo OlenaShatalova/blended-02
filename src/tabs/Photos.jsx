cbf963370bfe2456e644fa4e401ccc0e21f3d51a
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2024-10-06 13:09:12 +0300

    error

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -5,16 +5,26 @@ import { useEffect, useState } from 'react';
 export const Photos = () => {
   const [query, setQuery] = useState('');
   const [photos, setPhotos] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   const onSubmit = query => {
     setQuery(query);
+    setError(null);
   };
   useEffect(() => {
+    if (!query) {
+      return;
+    }
     const getData = async () => {
+      setIsLoading(true);
       try {
         const photos = await getPhotos(query, 1);
         setPhotos(photos.photos);
       } catch (error) {
-        console.log(error);
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     };
     getData();
@@ -24,6 +34,8 @@ export const Photos = () => {
     <>
       <Text textAlign="center">Let`s begin search 🔎</Text>
       <Form onSubmit={onSubmit} />
+      {isLoading && <p>Loading...</p>}
+      {error && <Text textAlign="center">{error}</Text>}
       <PhotosGallery photos={photos} />
     </>
   );
